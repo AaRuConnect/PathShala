66d9e9b3fa1fd56f30674a950e52875fe7cde3c8
srshakil2 <rh3642921@gmail.com>
2025-05-27 21:44:28 +0600

    added my branch and test added git

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+      <Button variant={"default"}>Click me</Button>
+      <p>this is App</p>
     </div>
   );
 }
