07e12c570f37fb16737bb01b0b418d64e939c1bc
AaRuConnectadmin <rubayetalam11@gmail.com>
2025-05-31 23:10:15 +0600

    chore: add another error button (#1)
    Co-authored-by: bakar31 <abubakar1808031@gmail.com>

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
+      <Button variant="destructive">Error</Button>
     </div>
   );
 }
