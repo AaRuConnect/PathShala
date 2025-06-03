6d4793c2ffcf9c1585739d570a7cb01b2e65f02d
Rubayet-Alom <rubayetalam11@gmail.com>
2025-06-03 02:47:34 -0700

    added new branch

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,8 +3,10 @@ import { Button } from "@/components/ui/button";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>Test Rubayet Alam branch</p>
       <Button>Click me</Button>
       <Button variant="destructive">Error</Button>
+      
     </div>
   );
 }
