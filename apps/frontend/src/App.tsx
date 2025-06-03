58e83bd95c85e9710f7f442bb2da1f85745c3c69
Rubayet-Alom <rubayetalam11@gmail.com>
2025-06-03 02:54:37 -0700

    sccess added branch

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <p>Test Rubayet Alam branch</p>
       <Button>Click me</Button>
       <Button variant="destructive">Error</Button>
       
